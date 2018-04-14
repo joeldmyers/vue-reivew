00b6607dc5cb6db76c26fabc4aeb3ec1ab46027e
Joel Myers <joeldmyers@gmail.com>
2018-04-14 13:14:28 -0400

    refactoring to v-html

--- first-app/src/components/FirstRoute.vue
+++ first-app/src/components/FirstRoute.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="test">
-    <h1>Test</h1>
+    <span v-html="title"></span>
+    <p>{{user.firstName}} {{user.lastName}}</p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'test'
+    name: 'test',
+    data() {
+      return {
+        title: '<h1>Hello there</h1>',
+        user: {
+          firstName: 'Joel',
+          lastName: 'Myers'
+        }
+      }
+    }
   }
 </script>
